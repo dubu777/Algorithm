af07e67058a46c71176386a9520fcc40a403c245
jaehyeon <122436486+dubu777@users.noreply.github.com>
2024-01-02 18:21:16 +0900

    [Silver I] Title: 그림, Time: 296 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,33 +1,27 @@
 from collections import deque
 
 def bfs(x, y):
-    a[x][y] = 0
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    w = 1
-    q = deque()
-    q.append([x, y])
+    q = deque([(x, y)])
+    cnt = 1
+    graph[x][y] = 0
     while q:
         x, y = q.popleft()
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and a[nx][ny] == 1:
-                q.append([nx, ny])
-                a[nx][ny] = 0
-                w += 1
-    return w
-
+        for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
+            nx, ny = dx + x, dy + y
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                cnt += 1
+                q.append((nx, ny))
+    return cnt
 
 n, m = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
-ans = 0
+graph = [list(map(int, input().split())) for _ in range(n)]
+max_size = 0
+count = 0
 for i in range(n):
     for j in range(m):
-        if a[i][j] == 1:
-            cnt += 1
-            ans = max(bfs(i, j), ans)
-
-print(cnt)
-print(ans)
+        if graph[i][j] == 1:
+            count += 1
+            max_size = max(bfs(i, j), max_size)
+print(count)
+print(max_size)
