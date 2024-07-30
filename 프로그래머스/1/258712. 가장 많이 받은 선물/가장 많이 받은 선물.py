54e559232f6ab146be89e9eb488425133587b084
jaehyeon <122436486+dubu777@users.noreply.github.com>
2024-07-30 17:19:53 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.13 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,42 +1,27 @@
 def solution(friends, gifts):
-    gifted = {} # gifted = {"friend 이름": {"선물 준 친구 이름": 이 친구에게 준 선물 개수}}
-    gift_idx = {} # 선물 지수 
-    # 딕셔너리 초기화
-    for friend in friends:
-        gifted[friend] = {}
-        gift_idx[friend] = 0
-    
+
+    l = len(friends)
+    lst = [[0] * l for _ in range(l)]
+    friend_dict = {name: index for index, name in enumerate(friends)}
+    gift_cnt = [0] * l
     for gift in gifts:
-        t, f = gift.split(' ') # t: 선물을 준 사람, f: 받은 사람
-        if f in gifted[t]:
-            gifted[t][f] += 1
-        else:
-            gifted[t][f] = 1
-        # 선물 지수 반영
-        gift_idx[t] += 1
-        gift_idx[f] -= 1
-    
-    # 각자 받게 될 선물 개수
-    will_get = [0 for _ in friends] # friends 리스트 순서대로 저장
-    for i in range(len(friends)):
-        curr = friends[i] # 인덱스 i에 해당하는 친구
-        for j in range(i+1, len(friends)):
-            another = friends[j] # 인덱스 j에 해당하는 친구
-            # curr가 another에게 준 선물 개수
-            a = gifted[curr][another] if another in gifted[curr] else 0 
-            # another가 curr에게 준 선물 개수
-            b = gifted[another][curr] if curr in gifted[another] else 0 
-            
-            if a > b: # curr가 선물을 더 많이 줬다면
-                will_get[i] += 1
-            elif a < b: # another가 선물을 더 많이 줬다면
-                will_get[j] += 1
-            elif a == b: # 둘이 선물을 주고 받은 개수가 같다면 선물 지수 확인
-                ai, bi = gift_idx[curr], gift_idx[another]
-                if ai > bi:
-                    will_get[i] += 1
-                elif ai < bi:
-                    will_get[j] += 1
-    
-    answer = max(will_get)
-    return answer
+        p, t = gift.split(" ")
+        lst[friend_dict[p]][friend_dict[t]] += 1
+
+    p = [0] * l
+    for i in range(l):
+        p[i] = sum(lst[i]) - sum([k[i] for k in lst])
+
+    for i in range(l):
+        for j in range(l):
+            if i == j:
+                continue
+            if lst[i][j] > lst[j][i]:
+                gift_cnt[i] += 1
+            elif lst[i][j] == lst[j][i]:
+                if p[i] > p[j]:
+                    gift_cnt[i] += 1
+
+
+    return max(gift_cnt)
+
